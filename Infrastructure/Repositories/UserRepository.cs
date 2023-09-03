b712392e679f046b42e9cf0978e32d1b9165374f
Administrator <Administrator@DESKTOP-5JALK0G>
2023-09-03 13:44:57 -0700

    Add project files.

---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿
+//using Domain.AppUsers;
+//using WebApplication2.Domain.Entity;
+//using WebApplication2.Infrastructure.Entity;
+
+//namespace Infrastructure.Data.Repositories
+//{
+//    public class UserRepository : RepositoryBase<User>
+//        , IUserRepository
+//    {
+//        public UserRepository(EcomOnlineOrderContext dbContext) : base(dbContext)
+//        {
+//        }
+//    }
+//}
